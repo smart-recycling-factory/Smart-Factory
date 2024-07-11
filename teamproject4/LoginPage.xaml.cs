7fdc7d782924b7dbdcf0351380a6d0eea4c11353
qkrskdusdlqslek <dnftksdurh10@daum.net>
2024-07-11 12:11:03 +0900

    Feat: 로그인 화면 추가

---
+++ teamproject4/LoginPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using teamproject4;
+
+namespace Smart_LoginPage
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : MetroWindow
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSignup_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            membership Membership = new membership();
+            Membership.Topmost = true;
+            Membership.ShowDialog();
+        }
+    }
+}
