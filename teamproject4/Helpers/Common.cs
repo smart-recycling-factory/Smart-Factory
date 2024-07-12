323eebdb97a79ad7cbd74f39aaeb61415b2b5b5a
HyungJuu <rmsdk617@naver.com>
2024-07-12 17:20:59 +0900

    Fix: textblock을 button으로 변경

--- teamproject4/Helpers/Common.cs
+++ teamproject4/Helpers/Common.cs
@@ -11,5 +11,9 @@ namespace teamproject4.Helpers
     {
         public static readonly string CONNSTRING = "Data Source=localhost;Initial Catalog=smart_factory;Persist Security Info=True;User ID=sa;Encrypt=False;" +
                                                    "Password = mssql_p@ss";
+
+        public static Boolean IsLogined = false;
+
+        public static String LogginedId = String.Empty;
     }
 }
